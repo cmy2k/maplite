fd5ff931af016ee7df67614199f08439e67889ae
Jeff Hicks <jmhicks@gmail.com>
2014-02-13 12:55:17 -0500

    Update mapLite to be a standalone project

--- jquery.maplite.js
+++ jquery.maplite.js
@@ -22,45 +22,69 @@
  * ------ METHODS ------
  * All methods are exposed via reflection through the widget reference
  * Example
- *   $( DIV_SELECTOR ).mapLite( "myMethodName", ARG1, ARG2, ... );
+ *   $( DIV_SELECTOR ).mapLite( 'myMethodName', ARG1, ARG2, ... );
  * 
  * Command methods (return this for chaining)
  * 
  * Getters (return the requested value)
  * 
- * ------- NOTES -------
- * projection: EPSG:900913
- * untis: m
- * 
  */
 
-function MapliteDataSource( url, name, pointStyle ) {
+$.nemac.MARKER_COLORS = {
+    RED: {hex: '#fb6254'},
+    GREEN: {hex: '#00e03c'},
+    BLUE: {hex: '#4462c8'},
+    CYAN: {hex: '#54d6d6'},
+    PURPLE: {hex: '#7d54fb'},
+    YELLOW: {hex: '#fcf357'}
+};
+
+function MapliteDataSource( url, name, color, projection, callback ) {
     this.url = url;
     this.name = name;
-    this.pointStyle = pointStyle;
+    this.color = color;
+    this.projection = projection;
+    this.callback = callback;
 }
 
 (function($){
-    // statics
-    var size = new OpenLayers.Size(14,24);
-    var offset = new OpenLayers.Pixel(-(size.w)/2, -size.h);
-    var defaultIconPath = 'static/icons/markers/24/fb6254.png';
-    var defaultMarker = new OpenLayers.Icon(defaultIconPath, size, offset);
+    // private constants
+    var SIZE = new OpenLayers.Size(14,24);
+    var OFFSET = new OpenLayers.Pixel(-(SIZE.w)/2, -SIZE.h);
+    var ICON_PATH = 'map/markers/24/';
+    var ICON_EXTENSION = '.png';
+    var UNITS = 'm';
+    var PROJECTION = 'EPSG:900913';
+    
+    function findIconPath( marker ) {
+        // translate to object, if string
+        if ( typeof marker === 'string' ) {
+            marker = marker.toUpperCase();
+            marker = $.nemac.MARKER_COLORS[marker];
+        }
+        
+        // provide default if marker isn't valid
+        if ( typeof marker === 'undefined' 
+                || typeof marker.hex === 'undefined' || marker.hex === null) {
+            marker = $.nemac.MARKER_COLORS.RED;
+        }
+        
+        return ICON_PATH + marker.hex.substring(1) + ICON_EXTENSION;
+    }
 
-
-    $.widget( "nemac.mapLite", {
+    $.widget( 'nemac.mapLite', {
         //
         // Defaults
         //
         options: {
             baseLayer: new OpenLayers.Layer.XYZ(
-                    "OSM (with buffer)",
+                    'OSM (with buffer)',
                     [
-                        "http://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
-                        "http://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
-                        "http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
+                        'http://a.tile.openstreetmap.org/${z}/${x}/${y}.png',
+                        'http://b.tile.openstreetmap.org/${z}/${x}/${y}.png',
+                        'http://c.tile.openstreetmap.org/${z}/${x}/${y}.png'
                     ], {
-                        transitionEffect: "resize",
+                        transitionEffect: 'resize',
                         buffer: 2,
                         sphericalMercator: true
                     }),
@@ -71,40 +95,66 @@ function MapliteDataSource( url, name, pointStyle ) {
         },
         
         //
+        // Private vars
+        //
+        layers: [],
+        map: null,
+        
+        //
         // Private methods
         //
         _create: function() {
-            // add the isBaseLayer property to the specified base layer, if not added
-            var baseLayer = $.extend( {}, this.options.baseLayer, { isBaseLayer: true });
-            // push the base layer into the layers array for cleaner initialization
-            var layers = $.merge( [], this.options.layers );
-            layers.push( baseLayer );
-            
-            var deferredLayers;
-            
-            // separate mapLite layers from passthrough layers
-            $.each( layers, function( index, value ) {
-                if ( value instanceof MapliteDataSource ) {
-                    deferredLayers.push( value );
-                    layers.splice( index, 1 );
-                }
-            });
-            
-            this.map = this._initMap();
-            map.zoomToExtent( this.options.extent, true );
-            
+            // prepare layers
+            this.layers = this._mergeBaseLayerWithLayers( this.options.baseLayer, this.options.layers );
+            var mapLayers = this._separateLayersByType( this.layers );
+
+            // init map
+            this.map = this._initMap( mapLayers.passthrough );
+            this.map.zoomToExtent( this.options.extent, true );
+
+            // add deferred layers
             var instance = this;
-            $.each( deferredLayers, function( index, value ) {
+            $.each( mapLayers.deferred, function( i, value ) {
                 instance._addMapliteData( value, instance.map );
             });
         },
         
-        _initMap: function() {
+        _mergeBaseLayerWithLayers: function( base, layers ) {
+            // add the isBaseLayer property to the specified base layer, if not added
+            var baseLayer = $.extend( {}, base, { isBaseLayer: true });
+            
+            // push the base layer into the layers array for cleaner initialization
+            var mergedLayers = $.merge( [], layers );
+            mergedLayers.push( baseLayer );
+            
+            return mergedLayers;
+        },
+        
+        _separateLayersByType: function( layers ) {
+            var passthroughLayers = [];
+            var deferredLayers = [];
+
+            // separate mapLite layers from passthrough layers
+            $.each( layers, function( i, value ) {
+                if ( value instanceof MapliteDataSource ) {
+                    deferredLayers.push( value );
+                } else {
+                    passthroughLayers.push( value );
+                }
+            });
+            
+            return {
+                deferred: deferredLayers,
+                passthrough: passthroughLayers
+            };
+        },
+        
+        _initMap: function( initialLayers ) {
             return new OpenLayers.Map({
                 div: this.element[0],
                 extent: this.options.extent,
-                units: "m",
-                layers: layers,
+                units: UNITS,
+                layers: initialLayers,
                 controls: [
                     new OpenLayers.Control.Navigation({
                         dragPanOptions: {
@@ -113,26 +163,69 @@ function MapliteDataSource( url, name, pointStyle ) {
                     })
                 ],
                 zoom: 4,
-                projection: new OpenLayers.Projection( "EPSG:900913" )
+                projection: new OpenLayers.Projection( PROJECTION )
             });
         },
         
-        _addMapliteData: function( layer, map ) {
+        _addMapliteData: function( mapliteLayer, map ) {
             var instance = this;
-            $.getJSON( layer.url, function( data ) {
-                map.addLayer( instance._translateJSON( layer, data ) );
+            
+            $.getJSON( mapliteLayer.url, function( points ) {
+                var pointsLayer = instance._translateJSON( mapliteLayer, points, instance.map.getProjectionObject() );
+                map.addLayer( pointsLayer );
             });
         },
         
-        _translateJSON: function( layer, data ) {
-            var pointsLayer = new OpenLayers.Layer.Markers(
-                    layer.name,
-                    {
-                        projection  : new OpenLayers.Projection("EPSG:900913"), 
-                        units       : "m"
-                    }
+        _translateJSON: function( mapliteLayer, points, mapProjection ) {
+            var markerIcon = new OpenLayers.Icon(
+                    findIconPath(mapliteLayer.color),
+                    SIZE,
+                    OFFSET
             );
             
+            var pointsLayer = new OpenLayers.Layer.Markers(
+                    mapliteLayer.name,
+                    {
+                        projection  : mapProjection, 
+                        units       : UNITS
+                    }
+            );
+
+            $.each( points, function( i, point ) {
+                var coordinates = new OpenLayers.LonLat( point.lon, point.lat );
+
+                if ( mapliteLayer.projection !== mapProjection ) {
+                    coordinates = coordinates.transform(
+                            mapliteLayer.projection,
+                            mapProjection
+                    );
+                }
+
+                var marker = new OpenLayers.Marker(
+                        coordinates,
+                        markerIcon.clone()
+                );
+        
+                if ( mapliteLayer.callback !== null && typeof mapliteLayer.callback === 'function' ) {
+                    marker.events.register( 'click', marker, function() { 
+                        mapliteLayer.callback( this, point );
+                    } );
+                    marker.events.register( 'touchstart', marker, function() { 
+                        mapliteLayer.callback( this, point );
+                    } );
+                }
+
+                pointsLayer.addMarker( marker );
+            });
+
+            return pointsLayer;
+        },
+        
+        //
+        // Public methods
+        //
+        zoomToMaxExtent: function() {
+            this.map.zoomToMaxExtent();
         }
     });
 
